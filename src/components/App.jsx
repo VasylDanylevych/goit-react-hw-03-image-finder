ebef169e134fd65730dba312a5fb3505532f4873
VasylDanylevych <ukrainec31@gmail.com>
2023-03-22 16:44:16 +0200

    add HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,72 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import ImgApi from '../services/getImage';
+import { Component } from 'react';
+import { LoadMoreButton } from './Button/Button';
+import Searchbar from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+
+const imgApi = new ImgApi();
+
+class App extends Component {
+	state = {
+		inputValue: '',
+		images: [],
+		showBtn: false,
+		isLoading: false,
+	};
+
+	componentDidUpdate(_, prevState) {
+		if (prevState.inputValue !== this.state.inputValue) {
+			this.setState({ showBtn: false, isLoading: true });
+			imgApi
+				.getImg(this.state.inputValue)
+				.then(data => {
+					if (data.hits.length === 0) throw new Error(Error);
+					const galleryLength = data.hits.length < 12;
+					this.setState({ showBtn: galleryLength ? false : true });
+
+					this.setState({
+						images: data.hits,
+					});
+				})
+				.catch(error => {
+					console.log(error);
+				})
+				.finally(() => {
+					this.setState({ isLoading: false });
+				});
+		}
+	}
+
+	onSubmit = inputValue => {
+		imgApi.resetPage();
+
+		this.setState({ images: [] });
+		this.setState({ inputValue });
+	};
+
+	onClick = () => {
+		imgApi.incrementPage();
+		imgApi.getImg(this.state.inputValue).then(data => {
+			this.setState(prevState => ({
+				images: [...prevState.images, ...data.hits],
+			}));
+		});
+	};
+
+	render() {
+		const { isLoading, showBtn } = this.state;
+		// console.log(this.state.images);
+
+		return (
+			<>
+				<Searchbar onSubmit={this.onSubmit} />
+				<ImageGallery images={this.state.images} />
+				{isLoading && <Loader></Loader>}
+				{showBtn && <LoadMoreButton onClick={this.onClick} />}
+			</>
+		);
+	}
+}
+
+export default App;
